b692debe3710661dea669c32380495a4b6ae2084
Boris Schrijver <bschrijver@schubergphilis.com>
2016-01-26 15:47:30 +0100

    Correct order of TABLE removal

--- cosmic-core/setup/db/db/schema-480to500-cleanup.sql
+++ cosmic-core/setup/db/db/schema-480to500-cleanup.sql
@@ -3,10 +3,10 @@
 --;
 
 # Remove NetApp plugin
+DROP TABLE IF EXISTS `cloud`.`netapp_lun`;
 DROP TABLE IF EXISTS `cloud`.`netapp_volume`;
 DROP TABLE IF EXISTS `cloud`.`netapp_pool`;
-DROP TABLE IF EXISTS `cloud`.`netapp_lun`;
 
 # Remove BigSwitch plugin
-DROP TABLE IF EXISTS `cloud`.`external_bigswitch_bcf_devices`;
 DROP TABLE IF EXISTS `cloud`.`external_bigswitch_vns_devices`;
+DROP TABLE IF EXISTS `cloud`.`external_bigswitch_bcf_devices`;
