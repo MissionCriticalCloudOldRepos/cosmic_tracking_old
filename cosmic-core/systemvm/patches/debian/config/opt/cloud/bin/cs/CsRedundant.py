35cee3104431ea1edae0ca0a77b3e2c56d33ee16
Remi Bergsma <github@remi.nl>
2016-03-17 19:31:02 +0100

    Start/stop passwd server on both gateway ip and local ip address

--- cosmic-core/systemvm/patches/debian/config/opt/cloud/bin/cs/CsRedundant.py
+++ cosmic-core/systemvm/patches/debian/config/opt/cloud/bin/cs/CsRedundant.py
@@ -241,6 +241,7 @@ class CsRedundant(object):
         interfaces = [interface for interface in self.address.get_interfaces() if interface.needs_vrrp()]
         for interface in interfaces:
             CsPasswdSvc(interface.get_gateway()).stop()
+            CsPasswdSvc(interface.get_ip()).stop()
 
         self.cl.set_fault_state()
         self.cl.save()
@@ -277,6 +278,7 @@ class CsRedundant(object):
         interfaces = [interface for interface in self.address.get_interfaces() if interface.needs_vrrp()]
         for interface in interfaces:
             CsPasswdSvc(interface.get_gateway()).stop()
+            CsPasswdSvc(interface.get_ip()).stop()
         CsHelper.service("dnsmasq", "stop")
 
         self.cl.set_master_state(False)
@@ -332,7 +334,10 @@ class CsRedundant(object):
         CsHelper.service("xl2tpd", "restart")
         interfaces = [interface for interface in self.address.get_interfaces() if interface.needs_vrrp()]
         for interface in interfaces:
+            # Listen on gateway and local ip address, as cloud-init uses the 'dhcp-server-identifier' address,
+            #  which unfortunately is not the gateway address.
             CsPasswdSvc(interface.get_gateway()).restart()
+            CsPasswdSvc(interface.get_ip()).restart()
 
         CsHelper.service("dnsmasq", "restart")
         self.cl.set_master_state(True)
