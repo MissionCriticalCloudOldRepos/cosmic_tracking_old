edbb5e4c5ff2aa40e4121cfb9ff59630414ceb53
Wilder Rodrigues <wrodrigues@schubergphilis.com>
2016-03-17 10:12:47 +0100

    Remove HyperV and VMware data from DB during upgrade from 4.8 to 5.0

--- cosmic-core/setup/db/db/schema-480to500-cleanup.sql
+++ cosmic-core/setup/db/db/schema-480to500-cleanup.sql
@@ -14,3 +14,19 @@ DROP TABLE IF EXISTS `cloud`.`external_bigswitch_bcf_devices`;
 # Remove Brocade plugin
 DROP TABLE IF EXISTS `cloud`.`brocade_network_vlan_map`;
 DROP TABLE IF EXISTS `cloud`.`external_brocade_vcs_devices`;
+
+# Remove HyperV and VMware templates
+DELETE FROM `cloud`.`vm_template` WHERE hypervisor_type = 'Hyperv';
+DELETE FROM `cloud`.`vm_template` WHERE hypervisor_type = 'VMware';
+DELETE FROM `cloud`.`hypervisor_capabilities` WHERE hypervisor_type = 'Hyperv';
+DELETE FROM `cloud`.`hypervisor_capabilities` WHERE hypervisor_type = 'VMware';
+
+# Remove HyperV and VMware global settings
+DELETE FROM `cloud`.`configuration` WHERE value = 'hyperv.guest.network.device';
+DELETE FROM `cloud`.`configuration` WHERE value = 'hyperv.private.network.device';
+DELETE FROM `cloud`.`configuration` WHERE value = 'hyperv.public.network.device';
+DELETE FROM `cloud`.`configuration` WHERE value = 'router.template.hyperv';
+DELETE FROM `cloud`.`configuration` WHERE value = 'router.template.vmware';
+
+# Remove HyperV related column on physical_network_traffic_types table
+ALTER TABLE `cloud`.`physical_network_traffic_types` DROP COLUMN `hyperv_network_label`;
