65539fb485b2eb5e6e3840cab317d6b3c714307a
Remi Bergsma <github@remi.nl>
2016-04-09 19:54:58 +0200

    Improve SSH log line format

--- cosmic-core/tools/marvin/marvin/sshClient.py
+++ cosmic-core/tools/marvin/marvin/sshClient.py
@@ -94,8 +94,8 @@ class SshClient(object):
         else:
             for strOut in output:
                 results.append(strOut.rstrip())
-        self.logger.debug("{Cmd: %s via Host: %s} {returns: %s}" %
-                          (command, str(self.host), results))
+        self.logger.debug("[SSH] Executing command via host %s: %s Output: %s" %
+                          (str(self.host), command, results))
         return results
 
     def createConnection(self):
@@ -110,10 +110,8 @@ class SshClient(object):
         except_msg = ''
         while self.retryCnt >= 0:
             try:
-                self.logger.debug("====Trying SSH Connection: Host:%s User:%s\
-                                   Port:%s RetryCnt:%s===" %
-                                  (self.host, self.user, str(self.port),
-                                   str(self.retryCnt)))
+                self.logger.debug("[SSH] Trying SSH Connection to host %s on port %s as user %s. RetryCount: %s" %
+                                  (self.host, str(self.port), self.user, str(self.retryCnt)))
                 if self.keyPairFiles is None:
                     self.ssh.connect(hostname=self.host,
                                      port=self.port,
@@ -129,7 +127,7 @@ class SshClient(object):
                                      timeout=self.timeout,
                                      look_for_keys=False
                                      )
-                self.logger.debug("===SSH to Host %s port : %s SUCCESSFUL==="
+                self.logger.debug("[SSH] Connection to host %s on port %s is SUCCESSFUL"
                                   % (str(self.host), str(self.port)))
                 ret = SUCCESS
                 break
@@ -179,7 +177,8 @@ class SshClient(object):
         except Exception as e:
             printException(e)
         finally:
-            self.logger.debug(" Host: %s Cmd: %s Output:%s" % (self.host, command, str(ret)))
+            self.logger.debug("[SSH] Connection to host %s on port %s is SUCCESSFUL"
+                              (str(self.host), command, str(ret)))
             return ret
 
     def scp(self, srcFile, destPath):
