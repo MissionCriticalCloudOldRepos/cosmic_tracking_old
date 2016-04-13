9f562069054fc62149996152fb613843985d94b0
Remi Bergsma <github@remi.nl>
2016-04-13 11:27:03 +0200

    Have patchviasocket.pl use socat to make it more reliable

--- cosmic-core/scripts/vm/hypervisor/kvm/patchviasocket.pl
+++ cosmic-core/scripts/vm/hypervisor/kvm/patchviasocket.pl
@@ -1,26 +1,4 @@
 #!/usr/bin/perl -w
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
-#############################################################
-# This script connects to the system vm socket and writes the
-# authorized_keys and cmdline data to it. The system VM then
-# reads it from /dev/vport0p1 in cloud_early_config
-#############################################################
 
 use strict;
 use Getopt::Std;
@@ -51,8 +29,16 @@ close FILE;
 $cmdline =~ s/%/ /g;
 my $msg = "pubkey:" . $key . "\ncmdline:" . $cmdline;
 
-my $socket = IO::Socket::UNIX->new(Peer=>$sockfile,Type=>SOCK_STREAM)
-    or die "ERROR: unable to connect to $sockfile - $^E\n";
-print $socket "$msg\n";
-close $socket;
-
+print "Sending: $msg\n";
+print "To: $sockfile\n";
+my $runCmd="echo \"$msg\n\" | /usr/bin/socat - UNIX-CONNECT:$sockfile";
+my $exit_code=system($runCmd);
+if($exit_code!=0)
+{
+  print "Command $runCmd failed with an exit code of $exit_code.\n";
+  exit($exit_code >> 8);
+}
+else
+{
+  print "Command $runCmd successful!\n";
+}
