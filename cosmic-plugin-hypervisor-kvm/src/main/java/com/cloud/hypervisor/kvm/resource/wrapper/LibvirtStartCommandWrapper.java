9d1965c3354ae094ece13363dcda79f1c2729055
Miguel Ferreira <miguelferreira@me.com>
2016-06-09 12:23:16 +0200

    Remove references to PXE

--- cosmic-plugin-hypervisor-kvm/src/main/java/com/cloud/hypervisor/kvm/resource/wrapper/LibvirtStartCommandWrapper.java
+++ cosmic-plugin-hypervisor-kvm/src/main/java/com/cloud/hypervisor/kvm/resource/wrapper/LibvirtStartCommandWrapper.java
@@ -4,9 +4,6 @@
 
 package com.cloud.hypervisor.kvm.resource.wrapper;
 
-import java.net.URISyntaxException;
-import java.util.List;
-
 import com.cloud.agent.api.Answer;
 import com.cloud.agent.api.StartAnswer;
 import com.cloud.agent.api.StartCommand;
@@ -22,119 +19,115 @@ import com.cloud.network.Networks.TrafficType;
 import com.cloud.resource.CommandWrapper;
 import com.cloud.resource.ResourceWrapper;
 import com.cloud.vm.VirtualMachine;
-
 import org.libvirt.Connect;
 import org.libvirt.DomainInfo.DomainState;
 import org.libvirt.LibvirtException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URISyntaxException;
+import java.util.List;
+
 @ResourceWrapper(handles = StartCommand.class)
 public final class LibvirtStartCommandWrapper extends CommandWrapper<StartCommand, Answer, LibvirtComputingResource> {
 
-  private static final Logger s_logger = LoggerFactory.getLogger(LibvirtStartCommandWrapper.class);
+    private static final Logger s_logger = LoggerFactory.getLogger(LibvirtStartCommandWrapper.class);
 
-  @Override
-  public Answer execute(final StartCommand command, final LibvirtComputingResource libvirtComputingResource) {
-    final VirtualMachineTO vmSpec = command.getVirtualMachine();
-    vmSpec.setVncAddr(command.getHostIp());
-    final String vmName = vmSpec.getName();
-    LibvirtVmDef vm = null;
+    @Override
+    public Answer execute(final StartCommand command, final LibvirtComputingResource libvirtComputingResource) {
+        final VirtualMachineTO vmSpec = command.getVirtualMachine();
+        vmSpec.setVncAddr(command.getHostIp());
+        final String vmName = vmSpec.getName();
+        LibvirtVmDef vm = null;
 
-    DomainState state = DomainState.VIR_DOMAIN_SHUTOFF;
-    final KvmStoragePoolManager storagePoolMgr = libvirtComputingResource.getStoragePoolMgr();
-    final LibvirtUtilitiesHelper libvirtUtilitiesHelper = libvirtComputingResource.getLibvirtUtilitiesHelper();
-    Connect conn = null;
-    try {
-      final NicTO[] nics = vmSpec.getNics();
+        DomainState state = DomainState.VIR_DOMAIN_SHUTOFF;
+        final KvmStoragePoolManager storagePoolMgr = libvirtComputingResource.getStoragePoolMgr();
+        final LibvirtUtilitiesHelper libvirtUtilitiesHelper = libvirtComputingResource.getLibvirtUtilitiesHelper();
+        Connect conn = null;
+        try {
+            final NicTO[] nics = vmSpec.getNics();
 
-      for (final NicTO nic : nics) {
-        if (vmSpec.getType() != VirtualMachine.Type.User) {
-          nic.setPxeDisable(true);
-        }
-      }
+            vm = libvirtComputingResource.createVmFromSpec(vmSpec);
+            conn = libvirtUtilitiesHelper.getConnectionByType(vm.getHvsType());
+            libvirtComputingResource.createVbd(conn, vmSpec, vmName, vm);
 
-      vm = libvirtComputingResource.createVmFromSpec(vmSpec);
-      conn = libvirtUtilitiesHelper.getConnectionByType(vm.getHvsType());
-      libvirtComputingResource.createVbd(conn, vmSpec, vmName, vm);
-
-      if (!storagePoolMgr.connectPhysicalDisksViaVmSpec(vmSpec)) {
-        return new StartAnswer(command, "Failed to connect physical disks to host");
-      }
-
-      libvirtComputingResource.createVifs(vmSpec, vm);
-
-      s_logger.debug("starting " + vmName + ": " + vm.toString());
-      libvirtComputingResource.startVm(conn, vmName, vm.toString());
-
-      for (final NicTO nic : nics) {
-        if (nic.isSecurityGroupEnabled() || nic.getIsolationUri() != null
-            && nic.getIsolationUri().getScheme().equalsIgnoreCase(IsolationType.Ec2.toString())) {
-          if (vmSpec.getType() != VirtualMachine.Type.User) {
-            libvirtComputingResource.configureDefaultNetworkRulesForSystemVm(conn, vmName);
-            break;
-          } else {
-            final List<String> nicSecIps = nic.getNicSecIps();
-            String secIpsStr;
-            final StringBuilder sb = new StringBuilder();
-            if (nicSecIps != null) {
-              for (final String ip : nicSecIps) {
-                sb.append(ip).append(":");
-              }
-              secIpsStr = sb.toString();
-            } else {
-              secIpsStr = "0:";
+            if (!storagePoolMgr.connectPhysicalDisksViaVmSpec(vmSpec)) {
+                return new StartAnswer(command, "Failed to connect physical disks to host");
             }
-            libvirtComputingResource.defaultNetworkRules(conn, vmName, nic, vmSpec.getId(), secIpsStr);
-          }
-        }
-      }
 
-      // pass cmdline info to system vms
-      if (vmSpec.getType() != VirtualMachine.Type.User) {
-        // wait and try passCmdLine for 5 minutes at most for CLOUDSTACK-2823
-        String controlIp = null;
-        for (final NicTO nic : nics) {
-          if (nic.getType() == TrafficType.Control) {
-            controlIp = nic.getIp();
-            break;
-          }
-        }
-        for (int count = 0; count < 30; count++) {
-          libvirtComputingResource.passCmdLine(vmName, vmSpec.getBootArgs());
-          // check router is up?
-          final VirtualRoutingResource virtRouterResource = libvirtComputingResource.getVirtRouterResource();
-          final boolean result = virtRouterResource.connect(controlIp, 1, 5000);
-          if (result) {
-            break;
-          }
-        }
-      }
+            libvirtComputingResource.createVifs(vmSpec, vm);
 
-      state = DomainState.VIR_DOMAIN_RUNNING;
-      return new StartAnswer(command);
-    } catch (final LibvirtException e) {
-      s_logger.warn("LibvirtException ", e);
-      if (conn != null) {
-        libvirtComputingResource.handleVmStartFailure(conn, vmName, vm);
-      }
-      return new StartAnswer(command, e.getMessage());
-    } catch (final InternalErrorException e) {
-      s_logger.warn("InternalErrorException ", e);
-      if (conn != null) {
-        libvirtComputingResource.handleVmStartFailure(conn, vmName, vm);
-      }
-      return new StartAnswer(command, e.getMessage());
-    } catch (final URISyntaxException e) {
-      s_logger.warn("URISyntaxException ", e);
-      if (conn != null) {
-        libvirtComputingResource.handleVmStartFailure(conn, vmName, vm);
-      }
-      return new StartAnswer(command, e.getMessage());
-    } finally {
-      if (state != DomainState.VIR_DOMAIN_RUNNING) {
-        storagePoolMgr.disconnectPhysicalDisksViaVmSpec(vmSpec);
-      }
+            s_logger.debug("starting " + vmName + ": " + vm.toString());
+            libvirtComputingResource.startVm(conn, vmName, vm.toString());
+
+            for (final NicTO nic : nics) {
+                if (nic.isSecurityGroupEnabled() || nic.getIsolationUri() != null
+                        && nic.getIsolationUri().getScheme().equalsIgnoreCase(IsolationType.Ec2.toString())) {
+                    if (vmSpec.getType() != VirtualMachine.Type.User) {
+                        libvirtComputingResource.configureDefaultNetworkRulesForSystemVm(conn, vmName);
+                        break;
+                    } else {
+                        final List<String> nicSecIps = nic.getNicSecIps();
+                        final String secIpsStr;
+                        final StringBuilder sb = new StringBuilder();
+                        if (nicSecIps != null) {
+                            for (final String ip : nicSecIps) {
+                                sb.append(ip).append(":");
+                            }
+                            secIpsStr = sb.toString();
+                        } else {
+                            secIpsStr = "0:";
+                        }
+                        libvirtComputingResource.defaultNetworkRules(conn, vmName, nic, vmSpec.getId(), secIpsStr);
+                    }
+                }
+            }
+
+            // pass cmdline info to system vms
+            if (vmSpec.getType() != VirtualMachine.Type.User) {
+                // wait and try passCmdLine for 5 minutes at most for CLOUDSTACK-2823
+                String controlIp = null;
+                for (final NicTO nic : nics) {
+                    if (nic.getType() == TrafficType.Control) {
+                        controlIp = nic.getIp();
+                        break;
+                    }
+                }
+                for (int count = 0; count < 30; count++) {
+                    libvirtComputingResource.passCmdLine(vmName, vmSpec.getBootArgs());
+                    // check router is up?
+                    final VirtualRoutingResource virtRouterResource = libvirtComputingResource.getVirtRouterResource();
+                    final boolean result = virtRouterResource.connect(controlIp, 1, 5000);
+                    if (result) {
+                        break;
+                    }
+                }
+            }
+
+            state = DomainState.VIR_DOMAIN_RUNNING;
+            return new StartAnswer(command);
+        } catch (final LibvirtException e) {
+            s_logger.warn("LibvirtException ", e);
+            if (conn != null) {
+                libvirtComputingResource.handleVmStartFailure(conn, vmName, vm);
+            }
+            return new StartAnswer(command, e.getMessage());
+        } catch (final InternalErrorException e) {
+            s_logger.warn("InternalErrorException ", e);
+            if (conn != null) {
+                libvirtComputingResource.handleVmStartFailure(conn, vmName, vm);
+            }
+            return new StartAnswer(command, e.getMessage());
+        } catch (final URISyntaxException e) {
+            s_logger.warn("URISyntaxException ", e);
+            if (conn != null) {
+                libvirtComputingResource.handleVmStartFailure(conn, vmName, vm);
+            }
+            return new StartAnswer(command, e.getMessage());
+        } finally {
+            if (state != DomainState.VIR_DOMAIN_RUNNING) {
+                storagePoolMgr.disconnectPhysicalDisksViaVmSpec(vmSpec);
+            }
+        }
     }
-  }
 }
