34dbb05221d120ced98b2875f51b04617834660c
Boris Schrijver <bschrijver@schubergphilis.com>
2016-06-15 00:03:24 +0200

    Move to SLF4J.

--- cosmic-plugin-user-authenticator-sha256salted/src/main/java/com/cloud/server/auth/SHA256SaltedUserAuthenticator.java
+++ cosmic-plugin-user-authenticator-sha256salted/src/main/java/com/cloud/server/auth/SHA256SaltedUserAuthenticator.java
@@ -25,7 +25,8 @@ import java.util.Map;
 import javax.inject.Inject;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.bouncycastle.util.encoders.Base64;
 
 import com.cloud.user.UserAccount;
@@ -35,7 +36,7 @@ import com.cloud.utils.component.AdapterBase;
 import com.cloud.utils.exception.CloudRuntimeException;
 
 public class SHA256SaltedUserAuthenticator extends AdapterBase implements UserAuthenticator {
-    public static final Logger s_logger = Logger.getLogger(SHA256SaltedUserAuthenticator.class);
+    public static final Logger s_logger = LoggerFactory.getLogger(SHA256SaltedUserAuthenticator.class);
     private static final String s_defaultPassword = "000000000000000000000000000=";
     private static final String s_defaultSalt = "0000000000000000000000000000000=";
     @Inject
